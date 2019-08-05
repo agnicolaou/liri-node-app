4a9bbde5e85792b124c8d05b2cf0e6b83a115ccc
Andreas Nicolaou <49968813+agnicolaou@users.noreply.github.com>
2019-08-05 19:21:02 -0400

    Added bands concert schedule.

--- liri.js
+++ liri.js
@@ -41,7 +41,37 @@ var getMeSpotify = function (songName) {
     )
 };
 
-getMeSpotify("longview");
+var getMyBands = function (artist) {
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+    axios.get(queryURL).then(
+        function (response) {
+            var jsonData = response.data;
+
+            if (!jsonData.length) {
+                console.log("No results found for " + artist);
+                return;
+            }
+            console.log("Upcoming concerts for " + artist + ":");
+
+            for (var i = 0; i < jsonData.length; i++) {
+                var show = jsonData[i];
+
+                console.log(
+                    show.venue.city +
+                    "," +
+                    (show.venue.region || show.venue.country) +
+                    "at" +
+                    show.venue.name +
+                    " " +
+                    moment(show.datetime).format("MM/DD/YYYY")
+                );
+            }
+        }
+    );
+};
 
 
 
+
+getMyBands("Greta Van Fleet");
