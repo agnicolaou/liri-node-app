dcb7ba5a4c75380292b6bda65264eaf4c8f44199
Andreas Nicolaou <49968813+agnicolaou@users.noreply.github.com>
2019-08-05 19:46:28 -0400

    DO WHAT IT SAYS fs added.  Works

--- liri.js
+++ liri.js
@@ -94,3 +94,18 @@ var getMeMovie = function (movieName) {
         }
     );
 };
+
+var doWhatItSays = function () {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        console.log(data);
+
+        var dataArr = data.split(",");
+        if (dataArr.length === 2) {
+            pick(dataArr[0], dataArr[1]);
+        } else if (dataArr.length === 1) {
+            pick(dataArr[0]);
+        }
+    });
+};
+
+doWhatItSays();
