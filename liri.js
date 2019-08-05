d76a1477b982a3516d6fad65f4429d0942708272
Andreas Nicolaou <49968813+agnicolaou@users.noreply.github.com>
2019-08-05 19:39:25 -0400

    Movie request working.

--- liri.js
+++ liri.js
@@ -72,6 +72,25 @@ var getMyBands = function (artist) {
 };
 
 
+var getMeMovie = function (movieName) {
+    if (movieName === undefined) {
+        movieName = "Mr. Nobody";
+    }
+    var urlHit = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey=trilogy";
 
+    axios.get(urlHit).then(
+        function (response) {
+            var jsonData = response.data;
 
-getMyBands("Greta Van Fleet");
+            console.log("Title: " + jsonData.Title);
+            console.log("Year: " + jsonData.Year);
+            console.log("Rated: " + jsonData.Rated);
+            console.log("IMDB Rating: " + jsonData.imdbRating);
+            console.log("Country: " + jsonData.Country);
+            console.log("Language: " + jsonData.Language);
+            console.log("Plot: " + jsonData.Plot);
+            console.log("Actors: " + jsonData.Actors);
+            console.log("Rotten Tomatoes Ratings: " + jsonData.Ratings[1].Value);
+        }
+    );
+};
